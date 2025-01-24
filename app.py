dbb6f28f3a67d8175e47dd834db68fa198176d04
brennolvs <brenno.niit@gmail.com>
2025-01-24 04:08:57 -0300

    Primeiro Commit

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = 'secret_key_for_session_management'
+
+# Lista para armazenar os clientes na fila
+fila = []
+
+# Rota principal para exibir a fila
+@app.route('/')
+def index():
+    is_barber = session.get('is_barber', False)
+    return render_template('index.html', fila=fila, is_barber=is_barber)
+
+# Rota para adicionar cliente à fila
+@app.route('/adicionar', methods=['POST'])
+def adicionar_cliente():
+    nome = request.form.get('nome')
+    telefone = request.form.get('telefone')  # Pode ser vazio
+    servico = request.form.get('servico')
+
+    # Verificando se o nome e o serviço estão preenchidos
+    if nome and servico:
+        # Se telefone não for preenchido, armazene como uma string vazia
+        telefone = telefone if telefone else ''
+        fila.append({"nome": nome, "telefone": telefone, "servico": servico})
+    return redirect(url_for('index'))
+
+# Rota para chamar o próximo cliente (somente barbeiro)
+@app.route('/chamar')
+def chamar_cliente():
+    if not session.get('is_barber', False):
+        return redirect(url_for('login_page'))
+
+    if fila:
+        proximo = fila.pop(0)
+        return render_template('index.html', fila=fila, mensagem=f"Chamar: {proximo['nome']} ({proximo['telefone']}) - Serviço: {proximo['servico']}", is_barber=True)
+    return render_template('index.html', fila=fila, mensagem="A fila está vazia.", is_barber=True)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print("Acesso a página de login")
+    if request.method == 'POST':
+        senha = request.form.get('senha')
+        if senha == 'barbeiro123':  # Senha fixa para o barbeiro
+            session['is_barber'] = True
+            return redirect(url_for('index'))
+        else:
+            # Mensagem de senha incorreta
+            mensagem = "Senha incorreta. Tente novamente."
+            return render_template('login.html', mensagem=mensagem)
+    return render_template('login.html')
+
+# Rota para logout do barbeiro
+@app.route('/logout')
+def logout():
+    session.pop('is_barber', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
