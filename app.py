a9812acd1f2437e8aae8ca5890eb543730d19c01
brennolvs <brenno.niit@gmail.com>
2025-01-28 00:42:53 -0300

    Adicionando novos serviços

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import os
 
 app = Flask(__name__)
 app.secret_key = 'secret_key_for_session_management'
@@ -58,4 +59,5 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
